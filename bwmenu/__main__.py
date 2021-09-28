fb2a04aaca505dbd37cc03bd2c891f02053ca173
haztecaso <adrianlattes@disroot.org>
2021-09-28 03:05:08 +0200

    reorganizing main

--- bwmenu/__main__.py
+++ bwmenu/__main__.py
@@ -7,12 +7,14 @@ Options:
       Show this help text and exit.
   -v, --version
       Show version information and exit.
+  -u, --url URL
+      Filter items that match this url basename.
   --bw-match
       Use bitwarden-cli to match urls instead of custom implementation.
   -c, --clear-cache
-      Clear cache files.
-  -C, --clear-items-cache
-      Only clear item list cache file.
+      Clear item list cache file.
+  -C, --clear-all-cache
+      Clear item list and session key cache files.
 """
 
 from docopt import docopt
@@ -22,42 +24,30 @@ from .utils import ProcessError
 from .bitwarden import BitWarden, AuthError
 from .rofi import select_item, error_message
 
-def run():
-    args = docopt(__doc__, version="bitwarden v0.1.1")
-    bw = BitWarden()
-    if args.get("--clear-cache"):
-        bw.session_cache.clear()
-        bw.list_cache.clear()
-    elif args.get("--clear-items-cache"):
-        bw.list_cache.clear()
-    qutebrowser = getenv("QUTE_MODE") == "command"
-    if qutebrowser:
-        items = bw.search_item_by_url(getenv("QUTE_URL"), args['--bw-match'])
+
+def run(args, bw, qute, url):
+    if url:
+        items = bw.search_item_by_url(url, args['--bw-match'])
     else:
         items = bw.item_list
     if len(items) == 0:
         error_message("No items found")
     else:
-        try:
-            item = items[0] if len(items) == 1 else select_item(items)
-        except ProcessError:
-            print("Item not selected...")
-        else:
-            item.type_all(qute = qutebrowser)
-
-
-def main_loop_catch_errors(n_retries:int):
-    if n_retries > 0:
-        try:
-            run()
-        except AuthError as e:
-            error_message(f'AuthError: {e.reason}')
-            n_retries -= 1
-            main_loop_catch_errors(n_retries)
+        try: item = select_item(items)
+        except ProcessError: print("Item not selected...")
+        else: item.type_all(qute=qute)
 
 
 def main():
-    main_loop_catch_errors(2)
+    args = docopt(__doc__, version="bitwarden v0.1.1")
+    bw = BitWarden()
+    qute = getenv("QUTE_MODE") == "command"
+    url = getenv("QUTE_URL") if qute else args.get("--url")
+    if not bw.clear_cache(**args) or url:
+        try:
+            run(args, bw, qute, url)
+        except AuthError as e:
+            error_message(f'AuthError: {e.reason}')
 
 
 if __name__ == "__main__":
